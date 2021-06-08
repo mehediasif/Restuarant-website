0f926faba524256d98314e85afd2a8f44b6ea055
Mohammad Mehadi Hasan <mohhas@st.amu.edu.pl>
2021-06-08 19:18:09 +0200

    3nd commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let chanda = window.pageYOffset;
 let body = document.querySelector('body');
-let nav = document.querySelector('.navbar')
+let nav = document.querySelector('.navbar');
+const findUSbtn = document.querySelector("#findUS");
 window.addEventListener("scroll", () =>{
     if(window.scrollY > 736){
         nav.style.backgroundColor= "black";
@@ -17,3 +18,8 @@ burger.addEventListener("click", () =>{
     console.log("clicked")
     navlist.classList.toggle('nav-active')
 })
+function seeLocations(){
+    const locateUrl = `https://www.google.com/maps/search/kebab+Center+/@54.1417054,16.41044,8z`;
+    window.open(locateUrl, '_blank');
+}
+findUSbtn.addEventListener("click", seeLocations);
